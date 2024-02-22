9f4a5023502ed3bd896eac5488ee5e7f0db83e62
Yehor Romaniuk <krasheg@gmail.com>
2024-02-22 16:24:12 +0200

    added form for comment, installed captcha

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from captcha.fields import CaptchaField
+
+
+class NewComment(forms.Form):
+    user_name = forms.CharField(max_length=30, required=True, label="User Name",
+                                widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(max_length=50, required=True, label="E-mail",
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
+    home_page = forms.URLField(max_length=256, required=False, label="Home page",
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    captcha = CaptchaField(label="CAPTCHA")
+    text = forms.CharField(max_length=1000, required=True, label="Text",
+                           widget=forms.Textarea(attrs={'class': 'form-control'}))
+    parent_id = forms.IntegerField(widget=forms.HiddenInput(attrs={'class': 'visually-hidden'}), label=False)
