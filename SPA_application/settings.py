2e372c556f6923bf92792ceb25a7e973a4cda680
Yehor Romaniuk <krasheg@gmail.com>
2024-02-23 17:45:39 +0200

    added pythonanywhere to allowed app

--- SPA_application/settings.py
+++ SPA_application/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'spaappdzencode.pythonanywhere.com']
 
 # Application definition
 
